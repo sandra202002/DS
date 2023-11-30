719af54d2d33014be4d7a9cf44ab4c393464d116
sandra santhosh <147963290+sandra202002@users.noreply.github.com>
2023-11-29 23:08:27 -1100

    Add files via upload

--- BFS.C
+++ BFS.C
@@ -1,107 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <conio.h>
+	int n,i,j,front=0,rear=0,x=1,v,flag=0;
+	int visited[10],queue[10],cost[10][10],newarr[10];
+	void bfs(int);
+void main()
+{
 
-#define MAX_SIZE 100
-int i,j;
-// Queue structure for BFS
-struct Queue {
-    int items[MAX_SIZE];
-    int front;
-    int rear;
-};
+	clrscr();
 
-// Function to initialize a queue
-struct Queue* createQueue() {
-    struct Queue* q = (struct Queue*)malloc(sizeof(struct Queue));
-    q->front = -1;
-    q->rear = -1;
-    return q;
-}
-
-// Function to check if the queue is empty
-int isEmpty(struct Queue* q) {
-    return q->front == -1;
-}
-
-// Function to add an item to the queue
-void enqueue(struct Queue* q, int value) {
-    if (q->rear == MAX_SIZE - 1) {
-	printf("Queue is full\n");
-    } else {
-	if (q->front == -1) {
-	    q->front = 0;
-	}
-	q->rear++;
-	q->items[q->rear] = value;
-    }
-}
-
-// Function to remove an item from the queue
-int dequeue(struct Queue* q) {
-    int item;
-    if (isEmpty(q)) {
-	printf("Queue is empty\n");
-	item = -1;
-    } else {
-	item = q->items[q->front];
-	q->front++;
-	if (q->front > q->rear) {
-	    q->front = q->rear = -1;
-	}
-    }
-    return item;
-}
-
-// Function to perform BFS on a graph represented by an adjacency matrix
-void bfs(int graph[MAX_SIZE][MAX_SIZE], int vertices, int startVertex) {
-    struct Queue* q = createQueue();
-    int visited[MAX_SIZE] = {0};
-
-    printf("Breadth-First Search starting from vertex %d:\n", startVertex);
-    printf("%d ", startVertex);
-    visited[startVertex] = 1;
-    enqueue(q, startVertex);
-
-    while (!isEmpty(q)) {
-	int currentVertex = dequeue(q);
-
-	for (i = 0;i < vertices; i++)
+	printf("Enter the number of nodes\n");
+	scanf("%d",&n);
+	 for(i=1;i<=n;++i)
 	 {
-	    if (graph[currentVertex][i] == 1 && !visited[i])
-	    {
-		printf("%d ", i);
-		visited[i] = 1;
-		enqueue(q, i);
-	    }
-	}
-    }
+		queue[i]=0;
+		visited[i]=0;
+	 }
 
-    printf("\n");
+	printf("Enter cost adjacency matrix\n");
+	for (i=1;i<=n;++i)
+	{
+		for(j=i;j<=n;++j)
+		{
+			printf("Enter cost( %d,%d )\n",i,j);
+			scanf("%d",&cost[i][j]);
+			cost[j][i]=cost[i][j];
+		}
+	}
+	for (i=1;i<=n;++i)
+	{
+		for(j=1;j<=n;++j)
+		{
+			printf("%d ",cost[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Enter the starting vertex\n");
+	scanf("%d",&v);
+	visited[v]=1;
+	front++;
+	rear++;
+	queue[rear]=v;
+	newarr[x]=v;
+	bfs(v);
+	printf("\nVisited array\n");
+	for(j=1;j<=n;++j)
+	{
+		if(visited[j]==1)
+			printf("%d ",j);
+		else
+		{
+			printf("\nNo Spanning tree\n");
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	{
+		printf("\nNew array\n");
+		for(j=1;j<=n;++j)
+		{
+			printf("%d ",newarr[j]);
+		}
+	}
+	getch();
 }
 
-int main() {
-    int vertices, startVertex;
-    int graph[MAX_SIZE][MAX_SIZE];
 
-    printf("Enter the number of vertices: ");
-    scanf("%d", &vertices);
-
-
-
-    printf("Enter the adjacency matrix:\n");
-    for (i=0;i<vertices;i++)
-    {
-	for ( j = 0; j < vertices; j++)
+void bfs(int v)
+{
+	for (i=1;i<=n;++i)
 	{
-	    scanf("%d", &graph[i][j]);
-	}
-    }
-
-    printf("Enter the starting vertex: ");
-    scanf("%d", &startVertex);
-
-    bfs(graph, vertices, startVertex);
-
-    getch();
+		if( cost[v][i]!=0 && visited[i]==0)
+		      {
+			rear++;
+			queue[rear]=i;
+			visited[queue[rear]]=1;
+			x++;
+			newarr[x]=queue[rear];
+		      }
+		}
+			if(front<=rear)
+			{
+			    bfs(queue[front++]);
+			}
 }
