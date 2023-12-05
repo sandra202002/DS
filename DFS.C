cdeef02817d2117532a19f648d40be3b4c281b44
sandra santhosh <147963290+sandra202002@users.noreply.github.com>
2023-12-04 19:10:55 -1100

    Add files via upload

--- DFS.C
+++ DFS.C
@@ -1,33 +1,70 @@
+#include <stdio.h>
+#include<conio.h>
 
-#include<stdio.h>
+#define MAX_VERTICES 20
+
+int n, i, j, visited[MAX_VERTICES], stack[MAX_VERTICES], cost[MAX_VERTICES][MAX_VERTICES], x = 0;
+
+void dfs(int);
+
+int main() {
+   int cr;
+   clrscr();
+    printf("\nEnter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the elements\n");
+    for (i = 0; i < n; i++) {
+	for (j = i; j < n; j++) {
+	    printf("\nposition(%d,%d)", i, j);
+	    scanf("%d", &cost[i][j]);
+	    cost[j][i] = cost[i][j];
+	}
+    }
+
+    for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++) {
+	    printf("%d\t", cost[i][j]);
+	}
+	printf("\n");
+    }
 
 
-int G[10][10],visited[10],n;
-void DFS(int);    //n is no of vertices and graph is sorted in array G[10][10]
-void main()
-{
-    int i,j;
-    clrscr();
-    printf("Enter number of vertices:\n");
+    printf("\nEnter the starting vertex: ");
+    scanf("%d", &cr);
+    visited[cr] = 1;
+   // printf("\nDFS Traversal: ");
+   // printf("%d ", cr);
+    stack[0] = cr;
+    dfs(cr);
 
-scanf("%d",&n);
-    //read the adjecency matrix
-printf("\nEnter adjecency matrix of the graph:");
-
-for(i=0;i<n;i++)
-       for(j=0;j<n;j++)
-scanf("%d",&G[i][j]);
-    //visited is initialized to zero
-   for(i=0;i<n;i++)
-        visited[i]=0;
-    DFS(0);
+    return 0;
 }
-void DFS()
-{
-    int j,i;
-printf("\n%d",i);
-    visited[i]=1;
-for(j=0;j<n;j++)
-       if(!visited[j]&&G[i][j]==1)
-            DFS(j);
+
+void dfs() {
+    int top = 0;
+    int popped[MAX_VERTICES]; // To store popped elements for printing later
+    int p = 0; // Index for storing popped elements
+    while (top >= 0) {
+        int current = stack[top];
+        int found = 0;
+        for (i = 0; i < n; i++) {
+            if (cost[current][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
+               // printf("%d ", i);
+                stack[++top] = i;
+                found = 1;
+                break;
+            }
+        }
+        if (!found) {
+            popped[p++] = stack[top]; // Store popped element
+            top--;
+        }
+    }
+
+    printf("\nPopping Order from Stack: ");
+    for (i = p - 1; i >= 0; i--) {
+        printf("%d ", popped[i]); // Print elements in popping order
+    }
 }
